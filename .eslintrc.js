501754aba7027d72e971bdf3f598246822970ad8
Rennan Cockles <rennancockles@gmail.com>
2020-03-01 18:15:45 -0300

    Add Vuetify, moment and axios plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    "indent": ["error", 4]
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        'plugin:vue/essential',
+        '@vue/standard'
+    ],
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        indent: ['error', 4]
+    }
 }
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,8 @@
-const path = require('path');
-
 module.exports = {
     devServer: {
-        open: true,
-        // port: 9090,
+        open: true
     },
-    chainWebpack: config => {
-        config.resolve.alias.set("@@", path.join(__dirname, "./src/components"));
-    },
+    transpileDependencies: [
+        'vuetify'
+    ]
 }
