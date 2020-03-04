513b8e98a87cedd82cc9689dd3e0a23a861415ca
Rennan Cockles <rennancockles@gmail.com>
2020-03-04 00:01:10 -0300

    Componentes com menu de contas e menu do usuário no toolbar

---
+++ src/components/nav/AccountsMenu.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-menu origin="center center" transition="scale-transition" bottom offset-y>
+        <template v-slot:activator="{ on }">
+            <v-btn
+            v-on="on"
+            color="secondary"
+            class="ma-2 white--text"
+            >
+                {{ activeAccount.name }}
+                <v-icon right dark>mdi-twitter-retweet</v-icon>
+            </v-btn>
+        </template>
+
+        <v-list>
+            <v-subheader>Selecione uma conta</v-subheader>
+            <v-list-item
+            v-for="account in accounts"
+            :key="account.name"
+            @click="setActive(account.name)"
+            >
+                <v-list-item-icon>
+                    <v-icon v-if="account.active" color="success">mdi-check-circle-outline</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                    <v-list-item-title v-text="account.name"></v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+        </v-list>
+    </v-menu>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+    name: 'AccountsMenu',
+    computed: {
+        ...mapGetters(['accounts', 'activeAccount'])
+    },
+    methods: {
+        ...mapActions(['setActive'])
+    }
+}
+</script>
---
+++ src/components/nav/UserMenu.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-menu origin="center center" transition="scale-transition" bottom offset-y>
+        <template v-slot:activator="{ on }">
+            <v-btn
+            v-on="on"
+            color="info"
+            class="ma-2 white--text"
+            >
+                {{ user.name }}
+                <v-icon right dark>mdi-chevron-down</v-icon>
+            </v-btn>
+        </template>
+
+        <v-list two-line>
+            <v-list-item key="config" router to="/">
+                <v-list-item-action><v-icon>mdi-cogs</v-icon></v-list-item-action>
+                <v-list-item-content><v-list-item-title><span>Configuração</span></v-list-item-title></v-list-item-content>
+            </v-list-item>
+            <v-list-item key="logout" router to="/">
+                <v-list-item-action><v-icon>mdi-exit-to-app</v-icon></v-list-item-action>
+                <v-list-item-content><v-list-item-title><span>Sair</span></v-list-item-title></v-list-item-content>
+            </v-list-item>
+        </v-list>
+    </v-menu>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+    name: 'AccountsMenu',
+    computed: {
+        ...mapGetters(['user'])
+    }
+}
+</script>
