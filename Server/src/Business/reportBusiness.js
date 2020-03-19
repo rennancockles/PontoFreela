a8535fc28913a5d8bc3df33449fe22cbe468212a
Rennan Cockles <rennancockles@gmail.com>
2020-03-19 20:49:42 -0300

    Método para validação horas

--- Server/src/Business/reportBusiness.js
+++ Server/src/Business/reportBusiness.js
@@ -2,6 +2,15 @@ import moment from 'moment'
 import reportDAO from '../DAL/reportDAO'
 import recordDAO from '../DAL/recordDAO'
 
+function validateRecords(records) {
+    for (let i = 1; i < records.length; i++) {
+        if (moment(records[i-1].time, 'HH:mm') >= moment(records[i].time, 'HH:mm'))
+            return false
+    }
+
+    return true
+}
+
 export default {
     findById: async (id) => {
         const report = await reportDAO.findById(id)
@@ -30,22 +39,21 @@ export default {
         const { accountId } = reportInput
         const { records } = reportInput
         const report = { date: reportInput.date, obs: reportInput.obs }
-        console.log(moment(records[0].time, 'HH:mm'))
-        console.log(moment(records[1].time, 'HH:mm'))
-        console.log(moment(records[0].time, 'HH:mm') < moment(records[1].time, 'HH:mm'))
+
+        if (!validateRecords(records)) throw new Error("Horas inválidas!")
         
-        // const reportId = await reportDAO.insert(report, accountId)
+        const reportId = await reportDAO.insert(report, accountId)
 
-        // if (reportId && reportId > 0) {
-        //     report.id = reportId
+        if (reportId && reportId > 0) {
+            report.id = reportId
 
-        //     for (let i = 0; i < records.length; i++) {
-        //         const recordId = await recordDAO.insert(records[i], reportId)
-        //         records[i].id = recordId
-        //     }
+            for (let i = 0; i < records.length; i++) {
+                const recordId = await recordDAO.insert(records[i], reportId)
+                records[i].id = recordId
+            }
 
-        //     return { report, records }
-        // }       
+            return { report, records }
+        }       
 
         return null
     }
