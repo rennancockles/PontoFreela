4b8f7134f4bf266f588774a0ebc1c465ee731358
Rennan Cockles <rennancockles@gmail.com>
2020-04-13 00:48:42 -0300

    Corrige arredondamento de hora

--- WebSite/src/store/getters.js
+++ WebSite/src/store/getters.js
@@ -1,7 +1,7 @@
 import Auth from '@/auth.js'
 
 function msToStringHour (ms) {
-    const hour = Math.round(ms / 36e5)
+    const hour = Math.floor(ms / 36e5)
     const hourStr = (hour < 10 ? '0' : '') + hour
     const minutes = Math.round(60 * ((ms / 36e5) % 1))
     const minutesStr = (minutes < 10 ? '0' : '') + minutes
